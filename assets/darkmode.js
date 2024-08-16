6bcced2a018cb88ddb0e14b27e2813a5f104b781
su6o-guy <150414506+su6o@users.noreply.github.com>
2024-08-16 22:46:42 +0530

    Add files via upload

---
+++ assets/darkmode.js
@@ -0,0 +1,24 @@
+// Theme Toggle JS start
+
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme_toggle')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', 'null')
+}
+
+if (darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+// Theme Toggle JS close
+
